fc820151ea63a5887b468a4f13e7a88a34c7b5bc
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2023-11-29 17:27:07 +0100

    Added the basic JS.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Hello People");
+function generateCountry(event) {
+  event.preventDefault();
+  let travelElement = document.querySelector("#travel");
+
+  new Typewriter("#travel", {
+    strings: "Italy, England",
+    autoStart: true,
+    delay: 30,
+    cursor: "",
+  });
+}
+
+let travelFormElement = document.querySelector("#travel-generate-form");
+travelFormElement.addEventListener("submit", generateCountry);
